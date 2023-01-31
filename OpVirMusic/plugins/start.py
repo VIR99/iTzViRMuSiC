fe6f93ffe491a5bbacd747c0cfe74cdc7832d577
VIR⁹⁹ <102230621+VIR99@users.noreply.github.com>
2023-02-01 00:13:03 +0530

    ViR⁹⁹

--- OpVirMusic/plugins/start.py
+++ OpVirMusic/plugins/start.py
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAEHHLdjtFkP2_MChgrLvIXudEFUO4FQVAACfQEAAqnduVYuBGfJkDQibi0E")
+                await message.reply_sticker("CAADBQADlAgAApu5eVelYsQH0GN7ZQI")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
