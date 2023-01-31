ac461246a1f95067b9e7ff84365917cc20f5f437
VIR⁹⁹ <102230621+VIR99@users.noreply.github.com>
2023-02-01 00:08:20 +0530

    ViR⁹⁹

--- OpVirMusic/plugins/start.py
+++ OpVirMusic/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEHHLdjtFkP2_MChgrLvIXudEFUO4FQVAACfQEAAqnduVYuBGfJkDQibi0E")
+            await message.reply_sticker("CAADBQADlAgAApu5eVelYsQH0GN7ZQI")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
